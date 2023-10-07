0b5e7d5da919dc8d5b8eb3d0641d42564ce7e2aa
Prosukov Oleksandr <122283728+ProsukovOleksandr@users.noreply.github.com>
2023-10-07 13:27:02 +0300

    firstUpdate

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,22 +1,29 @@
 import express from "express";
 import Joi from "joi";
-import contactsService from "../../models/contacts.js";
-
+import Contact from "../../models/contacts.js";
+import { isValidObjectId } from "mongoose";
 const router = express.Router();
 
 const contactsAddSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
 const contactsPutSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
+  favorite: Joi.boolean(),
 });
+
+const contactsUpdateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required()
+})
+
 router.get("/", async (req, res, next) => {
   try {
-    const result = await contactsService.listContacts();
+    const result = await Contact.find();
     res.json(result);
   } catch (error) {
     next(error);
@@ -26,7 +33,13 @@ router.get("/", async (req, res, next) => {
 router.get("/:contactId", async (req, res, next) => {
   try {
     const { contactId } = req.params;
-    const result = await contactsService.getContactById(contactId);
+
+    if (!isValidObjectId(contactId)) {
+      const error = new Error(`${contactId} is not valid id`);
+      error.status = 404;
+      throw error;
+    }
+    const result = await Contact.findById(contactId);
 
     if (!result) {
       const error = new Error(`Not found`);
@@ -53,7 +66,7 @@ router.post("/", async (req, res, next) => {
       error.status = 404;
       throw error;
     }
-    const result = await contactsService.addContact(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
   } catch (error) {
     next(error);
@@ -63,7 +76,14 @@ router.post("/", async (req, res, next) => {
 router.delete("/:contactId", async (req, res, next) => {
   try {
     const { contactId } = req.params;
-    const result = await contactsService.removeContact(contactId);
+
+    if (!isValidObjectId(contactId)) {
+      const error = new Error(`${contactId} is not valid id`);
+      error.status = 404;
+      throw error;
+    }
+
+    const result = await Contact.findByIdAndDelete(contactId);
     if (!result) {
       const error = new Error(`Not found`);
       error.status = 404;
@@ -81,13 +101,20 @@ router.delete("/:contactId", async (req, res, next) => {
 router.put("/:contactId", async (req, res, next) => {
   try {
     const { contactId } = req.params;
+
+    if (!isValidObjectId(contactId)) {
+      const error = new Error(`${contactId} is not valid id`);
+      error.status = 404;
+      throw error;
+    }
+
     const { error } = contactsPutSchema.validate(req.body);
     if (error) {
       const error = new Error("Wrong content type");
       error.status = 404;
       throw error;
     }
-    const result = await contactsService.updateContact(contactId, req.body);
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new:true, runValidators: true});
     if (!result) {
       const error = new Error(`Not found`);
       error.status = 404;
@@ -99,4 +126,30 @@ router.put("/:contactId", async (req, res, next) => {
   }
 });
 
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+
+    if (!isValidObjectId(contactId)) {
+      const error = new Error(`${contactId} is not valid id`);
+      error.status = 404;
+      throw error;
+    }
+    const { error } = contactsUpdateFavoriteSchema.validate(req.body);
+    if (error) {
+      const error = new Error("missing field favorite");
+      error.status = 404;
+      throw error;
+    }
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new:true, runValidators: true});
+    if (!result) {
+      const error = new Error(`Not found`);
+      error.status = 404;
+      throw error;
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 export default router;
